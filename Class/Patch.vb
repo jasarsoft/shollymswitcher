d5ec426feb973e56be37f5b8f15a35c8d6ff5e5b
jasarsoft <edinjasar14@gmail.com>
2016-10-28 16:25:33 +0200

    added new class patch

---
+++ Class/Patch.vb
@@ -0,0 +1,47 @@
+﻿Imports System.IO
+
+Public NotInheritable Class Patch
+    Inherits Switcher
+    
+    Private _name As String
+    Private _list As List(Of String)
+
+    Public Sub New()
+        _list = New List(Of String)
+    End Sub
+
+
+    Public Property Name As String
+        Get
+            Return _name
+        End Get
+        Set(value As String)
+            _name = value
+        End Set
+    End Property
+
+    Public ReadOnly Property List As List(Of String)
+        Get
+            Return _list
+        End Get
+    End Property
+
+    Public Function Read() As Boolean
+        Dim dirPath As String
+
+        _list.Clear()
+        dirPath = Directory.GetCurrentDirectory() & ConstSlash
+        dirPath += FolderPatch
+
+        If Directory.Exists(dirPath) Then
+            For Each dirName As String In Directory.GetDirectories(dirPath)
+                Dim dirInfo As New DirectoryInfo(dirName)
+                _list.Add(dirInfo.Name)
+            Next
+            Return True
+        End If
+
+        Return False
+    End Function
+
+End Class
