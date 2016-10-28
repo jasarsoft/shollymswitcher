5dc3627b004424f6e39524d6bc8b0e5487b2fd63
jasarsoft <edinjasar14@gmail.com>
2016-10-28 16:39:57 +0200

    new logo class

---
+++ Class/Logo.vb
@@ -0,0 +1,33 @@
+﻿Imports System.IO
+
+Public Class Logo
+    Inherits Switcher
+
+    Private _path As String
+
+    Public ReadOnly Property Path As String
+        Get
+            Return _path
+        End Get
+    End Property
+
+
+    Public Function Read(ByVal patchName As String, ByVal seasonName As String) As Boolean
+        Dim filePath As String
+
+        filePath = Directory.GetCurrentDirectory() & ConstSlash
+        filePath += FolderPatch & ConstSlash
+        filePath += patchName & ConstSlash
+        filePath += seasonName & ConstSlash
+        filePath += FolderImage & ConstSlash
+        filePath += FileLogo
+
+        If File.Exists(filePath) Then
+            _path = filePath
+            Return True
+        End If
+
+        Return False
+    End Function
+
+End Class
