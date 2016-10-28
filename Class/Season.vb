9db98b087654a952020a12a62113c572617da7f2
jasarsoft <edinjasar14@gmail.com>
2016-10-28 16:22:37 +0200

    added new class

---
+++ Class/Season.vb
@@ -0,0 +1,48 @@
+﻿Imports System.IO
+
+Public Class Season
+    Inherits Switcher
+
+    Private _name As String
+    Private _list As List(Of String)
+
+    Public Sub New()
+        _list = New List(Of String)
+    End Sub
+
+    Public Property Name As String
+        Get
+            Return _name
+        End Get
+        Set(value As String)
+            _name = value
+        End Set
+    End Property
+
+    Public ReadOnly Property List As List(Of String)
+        Get
+            Return _list
+        End Get
+    End Property
+
+    Public Function Read(ByVal patchName As String) As Boolean
+        Dim dirPath As String
+
+        _list.Clear()
+        dirPath = Directory.GetCurrentDirectory() & ConstSlash
+        dirPath += FolderPatch & ConstSlash
+        dirPath += patchName
+
+        If Directory.Exists(dirPath) Then
+            For Each dirName As String In Directory.GetDirectories(dirPath)
+                Dim dirInfo As New DirectoryInfo(dirName)
+                _list.Add(dirInfo.Name)
+            Next
+
+            Return True
+        End If
+
+        Return False
+    End Function
+
+End Class
