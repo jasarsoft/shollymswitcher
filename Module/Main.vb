22c288382c945cc6fbd702a07a619c85ac951491
jasarsoft <edinjasar14@gmail.com>
2016-11-12 17:53:07 +0100

    add main entry point

---
+++ Module/Main.vb
@@ -0,0 +1,17 @@
+﻿Module Main
+
+    'Public _edit As Edit
+
+    'Public Function Main() As Integer
+
+    '    Application.EnableVisualStyles()
+    '    Application.SetCompatibleTextRenderingDefault(False)
+    '    Application.Run(New FormMain)
+
+    '    Return 1
+    'End Function
+
+    'Sub New()
+    '    _edit = New Edit()
+    'End Sub
+End Module
