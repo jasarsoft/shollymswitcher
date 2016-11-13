000e9f0bb9d29d2bcb19433d79ee19b5425dd335
jasarsoft <edinjasar14@gmail.com>
2016-11-13 01:40:52 +0100

    add save and backup method

--- Class/Gameplay.vb
+++ Class/Gameplay.vb
@@ -7,6 +7,7 @@ Public NotInheritable Class Gameplay
     Private _list As List(Of String)
 
     Public Sub New()
+        _name = Nothing
         _list = New List(Of String)
     End Sub
 
@@ -47,6 +48,93 @@ Public NotInheritable Class Gameplay
         End If
 
         Return False
+    End Function
+
+    Public Function Save(ByVal patchName As String, ByVal seasonName As String, ByVal gameplayName As String) As Boolean
+        Dim fileSource As String
+        Dim fileDestionation As String
+
+        fileSource = FolderApp & ConstSlash
+        fileSource += FolderPatch & ConstSlash
+        fileSource += patchName & ConstSlash
+        fileSource += seasonName & ConstSlash
+        fileSource += FolderGameplay & ConstSlash
+        fileSource += gameplayName & ConstSlash
+        fileSource += FileGameplay
+
+        fileDestionation = FolderMyDoc & ConstSlash
+        fileDestionation += FolderSave & ConstSlash
+        fileDestionation += FileGameplay
+
+        Try
+            My.Computer.FileSystem.CopyFile(fileSource, fileDestionation, True)
+            Return True
+        Catch ex As Exception
+            Return False
+        End Try
+
+    End Function
+
+    Public Function Backup(ByVal patchName As String, ByVal seasonName As String, ByVal gameplayName As String) As Boolean
+        Dim dirBackup As String
+        Dim fileBackup As String
+
+        fileBackup = FolderMyDoc & ConstSlash
+        fileBackup &= FolderSave & ConstSlash
+        fileBackup &= FileGameplay
+
+        If patchName Is Nothing Or seasonName Is Nothing Or gameplayName Is Nothing Then
+            If File.Exists(fileBackup) Then
+                Dim dirTemp As String
+
+                dirTemp = FolderBackup & ConstSlash
+                dirTemp &= Date.Now().ToString().Replace(":", "-") & ConstSlash
+
+                Try
+                    My.Computer.FileSystem.CreateDirectory(dirTemp)
+                    My.Computer.FileSystem.CopyFile(fileBackup, dirTemp & FileGameplay)
+                    Return True
+                Catch ex As Exception
+                    Return False
+                End Try
+            End If
+
+            Return True
+        End If
+
+        dirBackup = FolderApp & ConstSlash
+        dirBackup &= FolderPatch & ConstSlash
+        dirBackup &= patchName & ConstSlash
+        dirBackup &= seasonName & ConstSlash
+        dirBackup &= FolderGameplay & ConstSlash
+        dirBackup &= gameplayName & ConstSlash
+
+        If Directory.Exists(dirBackup) And File.Exists(fileBackup) Then
+            Try
+                My.Computer.FileSystem.CopyFile(fileBackup, dirBackup & FileGameplay, True)
+                Return True
+            Catch ex As Exception
+                Return False
+            End Try
+        End If
+
+        If File.Exists(fileBackup) Then
+            Dim dirTemp As String
+
+            dirTemp = FolderBackup & ConstSlash
+            dirTemp &= Date.Now().ToString().Replace(":", "-") & ConstSlash
+
+            Try
+                My.Computer.FileSystem.CreateDirectory(dirTemp)
+                My.Computer.FileSystem.CopyFile(fileBackup, dirTemp & FileGameplay)
+                Return True
+            Catch ex As Exception
+                Return False
+            End Try
+        Else
+            Return True
+        End If
+
     End Function
 
 End Class
