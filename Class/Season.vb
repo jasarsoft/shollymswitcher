16e07e2c770fe692b0e02368ce4e6ddbc284ff3f
jasarsoft <edinjasar14@gmail.com>
2016-11-21 00:09:57 +0100

    edit season class
    add path property;
    edit read, save and check method

--- Class/Season.vb
+++ Class/Season.vb
@@ -4,12 +4,14 @@ Public Class Season
     Inherits Switcher
 
     Private _name As String
+    Private _path As String
     Private _lenght As Integer
     Private _list As List(Of String)
 
     Public Sub New()
         _lenght = 0
         _name = Nothing
+        _path = Nothing
         _list = New List(Of String)
     End Sub
 
@@ -19,7 +21,14 @@ Public Class Season
         End Get
         Set(value As String)
             _name = value
+            _path = _patch.Path & Slash & _name & Slash & FolderData
         End Set
+    End Property
+
+    Public ReadOnly Property Path As String
+        Get
+            Return _path
+        End Get
     End Property
 
     Public ReadOnly Property Lenght As Integer
@@ -34,17 +43,18 @@ Public Class Season
         End Get
     End Property
 
-    Public Function Read(ByVal patch As String) As Boolean
-        Dim dirPath As String
+    Public Function Read() As Boolean
         Dim msgText As String
         Dim msgTitle As New Title()
 
         _lenght = 0
         _list.Clear()
-        dirPath = FolderApp & Slash & FolderPatch & Slash & patch
-        
-        If Directory.Exists(dirPath) Then
-            For Each dirName As String In Directory.GetDirectories(dirPath)
+
+        If _patch.Check() Then
+            Dim dirList As String()
+            dirList = Directory.GetDirectories(_patch.Path)
+
+            For Each dirName As String In dirList
                 Dim dirInfo As New DirectoryInfo(dirName)
 
                 _lenght += 1
@@ -52,61 +62,43 @@ Public Class Season
             Next
 
             If _lenght = 0 Then
-                msgText = "For the selected patch there is not a season." & Environment.NewLine
+                msgText = "The selected patch does not contain any sesaon." & Environment.NewLine
                 msgText &= "Shollym Switcher is not installed properly or is damaged."
 
                 MessageBox.Show(msgText, msgTitle.Mistake, MessageBoxButtons.OK, MessageBoxIcon.Error)
                 Return False
             End If
-
             'Success read
             Return True
-        Else
-            msgText = "The selected patch does not exist." & Environment.NewLine
-            msgText &= "Shollym Switcher is not installed properly or is damaged."
-
-            MessageBox.Show(msgText, msgTitle.Mistake, MessageBoxButtons.OK, MessageBoxIcon.Error)
         End If
 
         Return False
     End Function
 
-    Public Function Save(ByVal patch As String)
-        Dim dirPath As String
-        Dim msgText As String
-        Dim msgTitle As New Title()
+    Public Function Save() As Boolean
+        
+        If _patch.Check() And _season.Check() Then
+            Try
+                My.Computer.FileSystem.CopyDirectory(_path, "..\", True)
+                Return True
+            Catch ex As Exception
+                Dim msgText As String
+                Dim msgTitle As New Title()
 
-        dirPath = FolderApp & Slash & FolderPatch & Slash
-        dirPath &= patch & Slash & _name & Slash & FolderData
+                msgText = "Data on the selected patch and season is not saved." & Environment.NewLine
+                msgText &= "Shollym Switcher does not have administrative privileges."
 
-        If Not Directory.Exists(dirPath) Then
-            msgText = "No data is available on the selected patch and season."
-            msgText &= "Shollym Switcher is not installed properly or is damaged."
-
-            MessageBox.Show(msgText, msgTitle.Mistake, MessageBoxButtons.OK, MessageBoxIcon.Error)
-            Return False
+                MessageBox.Show(msgText, msgTitle.Mistake, MessageBoxButtons.OK, MessageBoxIcon.Error)
+                Return False
+            End Try
         End If
 
-        Try
-            My.Computer.FileSystem.CopyDirectory(dirPath, "..\", True)
-            Return True
-        Catch ex As Exception
-            msgText = "Data on the selected patch and season is not saved." & Environment.NewLine
-            msgText &= "Shollym Switcher does not have administrative privileges."
-
-            MessageBox.Show(msgText, msgTitle.Mistake, MessageBoxButtons.OK, MessageBoxIcon.Error)
-            Return False
-        End Try
-
+        Return False
     End Function
 
-    Public Function Check(ByVal patch As String)
-        Dim dirPath As String
-
-        dirPath = FolderApp & Slash & FolderPatch & Slash
-        dirPath &= patch & Slash & _name & Slash & FolderData
-
-        If Not Directory.Exists(dirPath) Then
+    Public Function Check() As Boolean
+        
+        If Not Directory.Exists(_path) Then
             Dim msgText As String
             Dim msgTitle As New Title()
 
