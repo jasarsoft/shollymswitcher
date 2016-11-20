6602a7a53abac04ece055b03031a449b8770ee47
jasarsoft <edinjasar14@gmail.com>
2016-11-20 15:11:51 +0100

    main entry point

--- Module/Main.vb
+++ Module/Main.vb
@@ -1,15 +1,23 @@
-﻿Module Main
+﻿Public Module Main
 
     Public _edit As Edit
 
-    'Public Function Main() As Integer
+    <STAThread()> _
+    Public Function Main(ByVal cmdArgs() As String) As Integer
 
-    '    Application.EnableVisualStyles()
-    '    Application.SetCompatibleTextRenderingDefault(False)
-    '    Application.Run(New FormMain)
+        If cmdArgs.Length > 0 Then
+            Dim _about As New About()
+            Dim msgTitle As New Title()
 
-    '    Return 1
-    'End Function
+            MessageBox.Show(_about.Title & " does not accept arguments.", msgTitle.Warn, MessageBoxButtons.OK, MessageBoxIcon.Warning)
+        End If
+
+        Application.EnableVisualStyles()
+        Application.SetCompatibleTextRenderingDefault(False)
+        Application.Run(New FormMain)
+
+        Return 1
+    End Function
 
     Sub New()
         _edit = New Edit()
